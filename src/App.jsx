62478fd8dc5c3e1de9a6cf0470ca7fd79c341cab
Femi Adedokun <adedokunfemi14@gmail.com>
2023-10-27 14:17:23 -0700

    Arranging the laayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import { useState } from 'react'
 import './App.css'
+import Header from './component/Header/Header'
+import Menu from './component/Menu/Menu'
 
 function App() {
 
   return (
-    <h1 class="text-3xl font-bold underline">
-     Hello world!
-  </h1>
+    <div className='flex'>
+      <div className="">
+        <Menu />
+      </div>
+      <div>
+        <Header />
+      </div>
+    </div>
   )
 }
 

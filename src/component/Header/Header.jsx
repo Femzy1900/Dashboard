36411fd59e7cadafb3a0a84fa10a92ca60fdde14
Femi Adedokun <adedokunfemi14@gmail.com>
2023-10-27 16:15:05 -0700

    header done

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -4,13 +4,13 @@ const Header = () => {
   return (
     <div className=' m-6 flex basis-9/12'>
       <div className="flex items-center">
-        <input className='p-1 rounded-2xl text-end shadow-xl bg-white w-[300px]' type="search" placeholder='search' name="" id="" />
+        <input className='p-1 rounded-2xl text-end shadow-xl bg-white w-[395px]' type="search" placeholder='search' name="" id="" />
       </div>
-      <div className="flex  justify-end">
-        <div className=" rounded-full w-[50px] h-[50px] border-4 border-[#925FE2]"></div>
-        <div className="flex flex-col">
-            <h2>John Doe</h2>
-            <h2>3rd year</h2>
+      <div className="flex w-[40%] items-center justify-end">
+        <div className="m-1 rounded-full w-[60px] h-[50px] border-4 border-[#925FE2]"></div>
+        <div className="flex flex-col ">
+            <h2 className='text-black leading-none font-bold text-[16px]'>John Doe</h2>
+            <h2 className='font-light  '>3rd year</h2>
         </div>
       </div>
     </div>

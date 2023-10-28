6063b619540655624f49ea2b14c6e161a30946d8
Femi Adedokun <adedokunfemi14@gmail.com>
2023-10-28 15:29:41 -0700

    Sidebar

--- src/component/Menu/Menu.jsx
+++ src/component/Menu/Menu.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Cap from "../../assets/Cap.png"
+import {MdSpaceDashboard} from "react-icons/md"
+import {BsFillPenFill} from "react-icons/bs"
 
 const Menu = () => {
   return (
@@ -7,15 +9,20 @@ const Menu = () => {
         <div className="w-[128px] h-[128px] m-auto py-4 bg-[#925FE2]">
             <img className=' mt-7 rounded-[25px] bg-[#7b40da] p-4 ' src={Cap} alt="" />
         </div>
-        <div className="bg-[#925FE2] mt-[150px]">
-            <ul className='bg-[#925FE2] flex flex-col gap-[48px] font-bold font-serif text-white'>
-                <li className='bg-[#925FE2]'>Sidebar</li>
-                <li className='bg-[#925FE2]'>Sidebar</li>
-                <li className='bg-[#925FE2]'>Sidebar</li>
-                <li className='bg-[#925FE2]'>Sidebar</li>
-                <li className='bg-[#925FE2]'>Sidebar</li>
-                <li className='bg-[#925FE2]'>Sidebar</li>
+        <div className="bg-[#925FE2] mt-[100px]">
+            <ul className='bg-[#925FE2] flex flex-col gap-[35px] font-extrabold font-serif text-white'>
+                <li className='bg-[#925FE2] flex justify-center items-center text-[16px] gap-2'>< MdSpaceDashboard className='w-[35px] bg-[#925FE2]'/>Dashboard</li>
+                <li className='bg-[#925FE2] flex justify-center items-center text-[16px] gap-2'>< BsFillPenFill className='w-[25px] bg-[#925FE2]'/>Payment Info</li>
+                <li className='bg-[#925FE2] flex justify-center items-center text-[16px] gap-2'>< BsFillPenFill className='w-[35px] bg-[#925FE2]'/>Registration</li>
+                <li className='bg-[#925FE2] flex justify-center items-center text-[16px] gap-2'>< MdSpaceDashboard className='w-[35px] bg-[#925FE2]'/>Courses</li>
+                <li className='bg-[#925FE2] flex justify-center items-center text-[16px] gap-2'>< MdSpaceDashboard className='w-[35px] bg-[#925FE2]'/>Dashboard</li>
+                <li className='bg-[#925FE2] flex justify-center items-center text-[16px] gap-2'>< MdSpaceDashboard className='w-[35px] bg-[#925FE2]'/>Dashboard</li>
+                <li className='bg-[#925FE2] flex justify-center items-center text-[16px] gap-2'>< MdSpaceDashboard className='w-[35px] bg-[#925FE2]'/>Dashboard</li>
+                <li className='bg-[#925FE2] flex justify-center items-center text-[16px] gap-2'>< MdSpaceDashboard className='w-[35px] bg-[#925FE2]'/>Dashboard</li>
             </ul>
+        </div>
+        <div className="flex justify-center mt-[100px] font-extrabold font-serif text-white">
+            <li className='bg-[#925FE2] flex justify-center items-center text-[16px] gap-2'>< MdSpaceDashboard   className='w-[35px] bg-[#925FE2]'/>Dashboard</li>
         </div>
     </div>
   )

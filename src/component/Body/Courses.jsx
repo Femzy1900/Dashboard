8c8bcf20f4e2c42c52fcfc330bf9e8746aebea87
Femi Adedokun <adedokunfemi14@gmail.com>
2023-10-29 08:31:38 -0700

    Working on courses

--- src/component/Body/Courses.jsx
+++ src/component/Body/Courses.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Courses = () => {
   return (
     <div className='flex flex-col'>
-      <h1 className='font-bold text-[25px]'>Courses</h1>
+      <h1 className='font-bold text-[25px]'>Courses instructors</h1>
+      <div className="flex">
+        
+      </div>
     </div>
   )
 }

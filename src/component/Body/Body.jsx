10e37bcf4967d4121ccde3fac4c195cb0e51c076
Femi Adedokun <adedokunfemi14@gmail.com>
2023-10-28 17:36:58 -0700

    image importation

--- src/component/Body/Body.jsx
+++ src/component/Body/Body.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import Header from '../Header/Header'
 import Bodyhead from './Bodyhead'
+import Finance from './FInance'
+import Courses from './Courses'
 
 const Body = () => {
   return (
     <div className='flex flex-col'>
         <Header />
         <Bodyhead />
-        <div className="flex"></div>
+        <div className="flex m-6">
+            <div className="">
+                <Finance />
+            </div>
+            <div className="basis-2/6">
+                <Courses />
+            </div>
+        </div>
     </div>
   )
 }
---
+++ src/component/Body/FInance.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Finance = () => {
+  return (
+    <div className='flex flex-col'>
+        <h1>Finance</h1>
+      <div className="flex gap-6">
+        <div className="flex flex-col bg-white border px-[60px] py-[25px] rounded-lg justify-center items-center">
+            <img src="" alt="" />\
+            <h1>Payment</h1>
+            <p>Total Payable</p>
+        </div>
+        <div className="flex flex-col bg-white border px-[60px] py-[25px] rounded-lg justify-center items-center">
+            <img src="" alt="" />\
+            <h1>Payment</h1>
+            <p>Total Payable</p>
+        </div>
+        <div className="flex flex-col bg-white border px-[60px] py-[25px] rounded-lg justify-center items-center">
+            <img src="" alt="" />\
+            <h1>Payment</h1>
+            <p>Total Payable</p>
+        </div>
+      </div>
+      <div className="">
+        <h1>Enrolled Courses</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Finance
